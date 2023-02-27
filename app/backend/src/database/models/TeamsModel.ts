3f018007b44447fe5a0ae4dfd9b3e31d5eaab498
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2023-02-27 17:51:50 -0300

    Feat: migration and model Team

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,40 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Team extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    field: 'team_name',
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Team;
