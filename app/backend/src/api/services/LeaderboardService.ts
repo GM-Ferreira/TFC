fcabb5d7c68e664fdfcdaa60a15b51a39f5b9b17
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2023-03-06 12:55:08 -0300

    Feat: Leaderboard sort function

--- app/backend/src/api/services/LeaderboardService.ts
+++ app/backend/src/api/services/LeaderboardService.ts
@@ -9,6 +9,29 @@ export default class LeaderboardService implements IServiceLeaderboard {
   modelMatch: ModelStatic<Match> = Match;
   modelTeam: ModelStatic<Team> = Team;
 
+  static async sortScore(finalScore: IScore[]) {
+    // total points desc
+    finalScore.sort((a, b) => {
+      if ((b.totalPoints - a.totalPoints) === 0) {
+        if ((b.totalVictories - a.totalVictories) === 0) {
+          if (b.goalsBalance - a.goalsBalance === 0) {
+            if (b.goalsFavor - a.goalsFavor === 0) {
+              // 4º Gols sofridos.
+              return b.goalsOwn - a.goalsOwn;
+            }
+            // 3º Gols a favor
+            return b.goalsFavor - a.goalsFavor;
+          }
+          // 2º Saldo de gols
+          return b.goalsBalance - a.goalsBalance;
+        }
+        // 1º Total de Vitórias
+        return b.totalVictories - a.totalVictories;
+      }
+      return b.totalPoints - a.totalPoints;
+    });
+  }
+
   async leaderboard():Promise<IScore[]> {
     const teams = await this.modelTeam.findAll();
     const matches = await this.modelMatch.findAll({
@@ -19,6 +42,8 @@ export default class LeaderboardService implements IServiceLeaderboard {
       const score = new Score(team, matches);
       return score.getPoints();
     });
+
+    await LeaderboardService.sortScore(finalScore);
 
     return finalScore;
   }
